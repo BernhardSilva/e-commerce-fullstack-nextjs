5f43f61f49e54b674e7539b882121a6ebe9f2798
Bernhard Silva <bernhard.silva@gmail.com>
2023-08-04 11:38:31 -0400

    req headers test

--- next.config.js
+++ next.config.js
@@ -8,14 +8,14 @@ const nextConfig = {
 	// compiler: {
 	// 	removeConsole: process.env.NODE_ENV === 'production'
 	// },
-	async rewrites() {
-		return [
-			{
-				source: '/app/api/:path*',
-				destination: `${process.env.WHITE_LIST_URL}/:path*`
-			}
-		];
-	},
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: '/app/api/:path*',
+	// 			destination: `${process.env.WHITE_LIST_URL}/:path*`
+	// 		}
+	// 	];
+	// },
 	images: {
 		domains: ['res.cloudinary.com']
 	}
