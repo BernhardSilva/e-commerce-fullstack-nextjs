912114723829308eb740c6498d6503947cd7c539
Bernhard Silva <bernhard.silva@gmail.com>
2023-09-10 17:21:38 -0300

    auth layout responsive

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,24 +3,21 @@ import logo from '../../public/logo.png';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<>
-			<div
-				className='flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat'
-				style={{
-					backgroundImage: `url('/auth-bg.avif')`
-				}}
-			>
-				<div className='rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8'>
-					<div className='text-white'>
-						<div className='mb-8 flex flex-col items-center'>
-							<div className='rounded-xl p-3'>
-								<Image src={logo} height={350} width={350} alt='' />
-							</div>
-						</div>
-						{children}
+		<div
+			className='flex min-h-screen min-w-screen items-center justify-center bg-gray-900 bg-cover bg-no-repeat sm:bg-contain sm:bg-center'
+			style={{
+				backgroundImage: `url('/auth-bg.avif')`,
+				backgroundSize: 'cover'
+			}}
+		>
+			<div className='rounded-xl bg-gray-800 bg-opacity-50 py-5 px-0 sm:py-10 sm:px-10'>
+				<div className='text-white'>
+					<div className='mb-8 flex flex-col items-center'>
+						<Image src={logo} height={250} width={250} alt='logo' />
 					</div>
+					{children}
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
