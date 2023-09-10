40d56d2ca57f98b4c466c1f1568fe9e44affc63d
Bernhard Silva <bernhard.silva@gmail.com>
2023-09-10 16:57:25 -0300

    auth layout updated

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,18 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />;
+	return (
+		<div className='grid-flow-row text-center'>
+			You can use this email and password to sign in to the test app.
+			<p>
+				Mail: <b>test@einrot.com</b>
+			</p>
+			<p>
+				Password: <b>TestP4$$w0rd</b>
+			</p>
+			<div className='mt-5'>
+				<SignIn />
+			</div>
+		</div>
+	);
 }
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,26 @@
-export default function AuthLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    return (
-        <div className="grid place-items-center h-full w-full">
-            {children}
-        </div>
-    );
-};
+import Image from 'next/image';
+import logo from '../../public/logo.png';
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<div
+				className='flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat'
+				style={{
+					backgroundImage: `url('/auth-bg.avif')`
+				}}
+			>
+				<div className='rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8'>
+					<div className='text-white'>
+						<div className='mb-8 flex flex-col items-center'>
+							<div className='rounded-xl p-3'>
+								<Image src={logo} height={350} width={350} alt='' />
+							</div>
+						</div>
+						{children}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
